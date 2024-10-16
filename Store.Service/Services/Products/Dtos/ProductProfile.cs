a848ff090ab1926498aede17080065191b4570f4
Amany17Route <gg6229663@gmail.com>
2024-10-16 15:18:21 +0300

    Video 01: Apply Auto Mapper

---
+++ Store.Service/Services/Products/Dtos/ProductProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Store.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Service.Services.Products.Dtos
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+           CreateMap<Product ,ProductDto>()
+            .ForMember(dest => dest.BarndName, option => option.MapFrom(src => src.Brand.Name))
+            .ForMember(dest => dest.TypeName, option => option.MapFrom(src => src.Type.Name));
+        }
+
+
+    }
+}
--- Store.Service/Services/Products/ProductService.cs
+++ Store.Service/Services/Products/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Store.Data.Entity;
 using Store.Repository.Interfaces;
 using Store.Repository.UnitOfWork;
@@ -8,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Store.Service.Services.Products
 {
@@ -15,12 +17,13 @@ namespace Store.Service.Services.Products
     {
 
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-
-        public ProductService(IUnitOfWork unitOfWork)
+        public ProductService(IUnitOfWork unitOfWork, IMapper mapper)
         {
 
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         public async Task<IReadOnlyList<BraandTypeDetailsDto>> GetAllBrandsAsync()
@@ -28,14 +31,8 @@ namespace Store.Service.Services.Products
 
             var brands = await _unitOfWork.Repository<ProductBrand, int>().GetAllAsync();
 
-            IReadOnlyList<BraandTypeDetailsDto> MappedBrands = brands.Select(x => new BraandTypeDetailsDto
+            var MappedBrands = _mapper.Map<IReadOnlyList<BraandTypeDetailsDto>>(brands);
 
-            {
-
-                Id = x.Id,
-                CreatedAt = x.CreatedAt,
-                Name = x.Name
-            }).ToList();
             return MappedBrands;
         }
 
@@ -43,20 +40,8 @@ namespace Store.Service.Services.Products
         {
 
             var products = await _unitOfWork.Repository<Product, int>().GetAllAsync();
+            var MappedProducts = _mapper.Map<IReadOnlyList<ProductDto>>(products);
 
-            var MappedProducts = products.Select(x => new ProductDto
-
-            {
-
-                Id = x.Id,
-                Name = x.Name,
-                BarndName = x.Brand.Name,
-                TypeName = x.Type.Name,
-                CreatedAt = x.CreatedAt,
-                Description = x.Description,
-                PicturUrl = x.ImageUrl
-
-            }).ToList();
 
             return MappedProducts;
         }
@@ -67,15 +52,8 @@ namespace Store.Service.Services.Products
         {
 
             var types = await _unitOfWork.Repository<ProductType, int>().GetAllAsync();
+            var mappedTypes = _mapper.Map<IReadOnlyList<BraandTypeDetailsDto>>(types);
 
-            var mappedTypes = types.Select(x => new BraandTypeDetailsDto
-            {
-
-
-                Id = x.Id,
-                Name = x.Name,
-                CreatedAt = x.CreatedAt
-            }).ToList();
 
             return mappedTypes;
         }
@@ -93,18 +71,12 @@ namespace Store.Service.Services.Products
             if (product is null)
                 throw new Exception("Product not Found");
 
-            var MappedProduct = new ProductDto
-            {
-                Id = product.Id,
-                Name = product.Name,
-                BarndName = product.Brand.Name,
-                CreatedAt = product.CreatedAt,
-                Description = product.Description,
-                PicturUrl = product.ImageUrl,
-                Price = product.Price,
-                TypeName = product.Type.Name
-            };
+
+            var MappedProduct = _mapper.Map<ProductDto>(product);
+
             return MappedProduct;
+
+
         }
     }
 }
