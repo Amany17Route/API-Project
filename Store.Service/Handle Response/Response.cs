83b22360a8259bfa12d2e3de298c2c88f0f7a13f
Amany17Route <gg6229663@gmail.com>
2024-10-20 22:12:00 +0300

    Video 05 : Middleware Classes And Strucure

---
+++ Store.Service/Handle Response/Response.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Service.Handle_Response
+{
+    public class Response
+    {
+        public Response(int statusCode, string? message = null)
+        {
+            StatusCode = statusCode;
+            Message = GetDefaultMessageForStatusCode(StatusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        => statusCode switch
+        {
+            100 => "Continue",
+            101 => "Switching Protocols",
+            200 => "OK",
+            201 => "Created",
+            202 => "Accepted",
+            203 => "Non-Authoritative Information",
+            204 => "No Content",
+            205 => "Reset Content",
+            206 => "Partial Content",
+            300 => "Multiple Choices",
+            301 => "Moved Permanently",
+            302 => "Found",
+            303 => "See Other",
+            304 => "Not Modified",
+            305 => "Use Proxy",
+            307 => "Temporary Redirect",
+            400 => "Bad Request",
+            401 => "Unauthorized",
+            403 => "Forbidden",
+            404 => "Not Found",
+            405 => "Method Not Allowed",
+            406 => "Not Acceptable",
+            408 => "Request Timeout",
+            409 => "Conflict",
+            500 => "Internal Server Error",
+            501 => "Not Implemented",
+            502 => "Bad Gateway",
+            503 => "Service Unavailable",
+            504 => "Gateway Timeout",
+            _ => "Unknown Status Code"
+        };
+
+    }
+}
