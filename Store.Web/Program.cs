a4c33d673a3f0ae92684990c04a04db5ead10221
Amany17Route <gg6229663@gmail.com>
2024-10-20 23:31:17 +0300

    Video 07 : Program refactor

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -5,6 +5,7 @@ using Store.Repository.Interfaces;
 using Store.Repository.UnitOfWork;
 using Store.Service.Services.Products;
 using Store.Service.Services.Products.Dtos;
+using Store.Web.Extensions;
 using Store.Web.Helper;
 using Store.Web.MiddleWare;
 
@@ -23,15 +24,18 @@ namespace Store.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<IUnitOfWork , UnitOfWork>();
-            builder.Services.AddScoped<IProductService , ProductService>();
-            builder.Services.AddAutoMapper(typeof(ProductProfile));
+          //  builder.Services.AddScoped<IUnitOfWork , UnitOfWork>();
+           // builder.Services.AddScoped<IProductService , ProductService>();
+            //builder.Services.AddAutoMapper(typeof(ProductProfile));
             builder.Services.AddDbContext<StoreDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
-            } ); 
+            } );
 
+
+
+            builder.Services.ApplicationServices();
             var app = builder.Build();
 
 
