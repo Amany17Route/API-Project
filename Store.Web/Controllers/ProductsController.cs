55bfb854e5ba0f611aeea1274e540063ff26eb52
Amany17Route <gg6229663@gmail.com>
2024-10-26 20:28:25 +0300

    Video 03: Cache Attribute

--- Store.Web/Controllers/ProductsController.cs
+++ Store.Web/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Store.Service.Services.Products.Dtos;
 using Store.Service.Services.Products;
 using Store.Repository.Specification.ProductSpecs;
+using Store.Web.Helper;
 
 namespace Store.Web.Controllers
 {
@@ -25,13 +26,14 @@ namespace Store.Web.Controllers
         => Ok(await _productService.GetAllBrandsAsync());
 
         [HttpGet]
+      
 
         public async Task<ActionResult<IReadOnlyList<BraandTypeDetailsDto>>> GetAllTypes()
         => Ok(await _productService.GetAllTypesAsync());
 
 
         [HttpGet]
-
+        [Cache(10)]
         public async Task<ActionResult<IReadOnlyList<ProductDto>>> GetAllProducts([FromQuery]ProductSpecification input)
         => Ok(await _productService.GetAllProductsAsync(input));
 
